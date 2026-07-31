66182ee97767e6491a2f0c292645370459050d5c
komalimonikanageswari <komalimonikanageswari@gmail.com>
2026-07-31 15:18:16 +0530

    Time: 4281 ms (6.31%) | Memory: 19.3 MB (58.33%) - LeetSync

---
+++ 2037-count-square-sum-triples/count-square-sum-triples.py
@@ -0,0 +1,9 @@
+class Solution:
+    def countTriples(self, n: int) -> int:
+        count = 0 
+        for i in range(1,n+1):
+            for j in range(i+1,n+1):
+                for k in range(j+1,n+1):
+                    if i**2 + j**2 == k**2 :
+                        count += 1 
+        return count * 2
